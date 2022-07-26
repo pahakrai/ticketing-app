6d02b73b992d6cbe6491331402b4574b01283f9a
pahakrai <tulakra@gmail.com>
2022-07-26 16:36:46 +0800

    mongo memory server update

--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -21,7 +21,7 @@ beforeAll(async () => {
     process.env.JWT_KEY = 'asdfasdf';
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-    mongo = new MongoMemoryServer();
+    mongo = MongoMemoryServer.create();
     const mongoUri = await mongo.getUri();
     await mongoose.connect(mongoUri, {
         // useNewUrlParser: true,
